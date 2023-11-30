f06a90d949c01c032ee21a1b25d8870b67bea68f
Himanshu Singh <chamyalhemsingh@gmail.com>
2023-11-30 10:53:05 +0530

    Worked towards making a image slider for homePage

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,5 +1,59 @@
-const Carousel = (slides) => {
-  return <div>{slides.map((slide) => {})}</div>;
+import { useEffect, useState } from "react";
+import { AiFillLeftCircle, AiFillRightCircle } from "react-icons/ai";
+
+interface propCarouselType {
+  images: string[];
+}
+const Carousel = ({ images }: propCarouselType) => {
+  const [index, setIndex] = useState(0);
+  const goPrev = () => {
+    console.log(index);
+    setIndex((prevInd) => (prevInd === 0 ? images.length - 1 : prevInd - 1));
+  };
+
+  const goNext = () => {
+    setIndex((prevInd) => (prevInd === images.length - 1 ? 0 : prevInd + 1));
+  };
+
+  const goToDot = (ind: number) => {
+    setIndex(ind);
+  };
+  // setInterval(goNext, 5000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      goNext();
+    }, 3000);
+
+    return () => clearInterval(interval);
+  });
+  const arrowStyles =
+    "text-gray-400 pt-[10px] pr-[8px] pb-[8px] pl-[13px] absolute top-[40%] bottom-0 mx-auto my-[10px] cursor-pointer";
+  return (
+    <div className="relative duration-700 ease-in-out">
+      <img
+        src={images[index]}
+        className="h-[75vh] w-full bg-contain duration-700 ease-in-out"
+      />
+      <div onClick={goPrev} className={`${arrowStyles} left-0`}>
+        <AiFillLeftCircle className="h-[30px] w-[30px]" />
+      </div>
+      <div onClick={goNext} className={`${arrowStyles} right-0 `}>
+        <AiFillRightCircle className="h-[30px] w-[30px]" />
+      </div>
+      <div className="flex gap-3 justify-center absolute bottom-2 left-[45%] z-30">
+        {images.map((_, ind) => (
+          <div
+            onClick={() => {
+              goToDot(ind);
+            }}
+            className={`w-3 h-3 rounded-full ${
+              ind == index ? "bg-red-500" : "bg-black"
+            }`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import womenImage from "../assets/images/womenShoes.jpeg";
 import kidsImage from "../assets/images/kids-shoes.jpg";
 import FeaturedShoes from "../components/Featured/Featured";
 import f1 from "../assets/images/converse1.jpg";
+import Carousel from "../components/Carousel/Carousel";
 
 const categories = [
   {
@@ -25,10 +26,20 @@ const categories = [
     button: "Shop Kids",
   },
 ];
+const images = [
+  "https://images.pexels.com/photos/169647/pexels-photo-169647.jpeg?auto=compress&cs=tinysrgb&w=600",
+  "https://images.pexels.com/photos/313782/pexels-photo-313782.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/773471/pexels-photo-773471.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/672532/pexels-photo-672532.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/632522/pexels-photo-632522.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/777059/pexels-photo-777059.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+];
 const HomePage = () => {
   return (
     <div className="flex flex-col">
-      <div className="h-[50vh]">Banner</div>
+      <div className="h-[80vh]">
+        <Carousel images={images} />
+      </div>
       <div className="flex flex-col items-center">
         <div className="w-[70vw] flex gap-6">
           {categories.map((category) => {
@@ -37,6 +48,7 @@ const HomePage = () => {
                 img={category.img}
                 about={category.about}
                 button={category.button}
+                key={category.img}
               />
             );
           })}
